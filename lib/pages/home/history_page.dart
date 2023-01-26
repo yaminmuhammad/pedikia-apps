402ba19fd11b8ce431a87e50f63d6c63535bc2f2
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-01-26 18:19:08 +0700

    add lottie animation

--- lib/pages/home/history_page.dart
+++ lib/pages/home/history_page.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:pedikia/theme.dart';
 
 class HistoryPage extends StatelessWidget {
   const HistoryPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'History Page',
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: ListView(
+            children: [
+              // Load a Lottie file from your assets
+              Container(
+                margin: EdgeInsets.only(top: 130),
+                child: Lottie.asset('assets/not_found.json',
+                    alignment: Alignment.bottomCenter),
+              ),
+              Center(
+                child: Container(
+                  margin: EdgeInsets.all(30),
+                  child: Text(
+                    'Tidak ada Riwayat',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 20,
+                      fontWeight: bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
