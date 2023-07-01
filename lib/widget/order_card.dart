269097f8e27424ad07fdb7f5afccb883bb765b9a
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-07-01 11:53:40 +0700

    fix history

--- lib/widget/order_card.dart
+++ lib/widget/order_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:pedikia/models/order_model.dart';
 import 'package:pedikia/providers/order_provider.dart';
 import 'package:pedikia/theme.dart';
@@ -18,50 +19,166 @@ class OrderCard extends StatelessWidget {
       margin: EdgeInsets.only(
         top: 20,
       ),
-      padding: EdgeInsets.only(
-        top: 10,
-        left: 12,
-        bottom: 14,
-        right: 20,
-      ),
+      padding: EdgeInsets.all(10),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12),
         color: secondaryColor,
       ),
-      child: Row(
+      child: Column(
         children: [
-          Text(
-            // trSuccess.therapist.name,
-            order.createdAt.toLocal().toString(),
-            style: primaryTextStyle.copyWith(
-              fontWeight: semiBold,
-            ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Dipesan tgl : '),
+              Text(
+                DateFormat('dd-MM-yyyy').format(order.createdAt.toLocal()),
+                style: primaryTextStyle.copyWith(
+                  fontWeight: semiBold,
+                ),
+              ),
+            ],
           ),
           SizedBox(
-            width: 12,
+            height: 12,
           ),
-          Text(
-            order.address,
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Jam             : '),
+              Text(
+                DateFormat('HH:mm:ss').format(order.createdAt.toLocal()),
+                style: primaryTextStyle.copyWith(
+                  fontWeight: semiBold,
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  order.status,
+          SizedBox(
+            height: 12,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Alamat        : '),
+              SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                child: Text(
+                  order.address,
                   style: primaryTextStyle.copyWith(
                     fontWeight: semiBold,
                   ),
                 ),
-                // Text(
-                //   '   Rp ${service.price.toStringAsFixed(0).replaceAll('.', ',')}',
-                //   style: priceTextStyle,
-                // ),
-              ],
-            ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Pembayaran : '),
+              Text(
+                order.payment,
+                style: primaryTextStyle.copyWith(
+                  fontWeight: semiBold,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Biaya Admin : '),
+              Text(
+                'Rp ${order.extraPrice.toStringAsFixed(0).replaceAll('.', ',')}',
+                style: primaryTextStyle.copyWith(
+                  fontWeight: semiBold,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Status : '),
+              Container(
+                width: 100,
+                height: 25,
+                padding: EdgeInsets.only(
+                  left: 4,
+                  right: 4,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: _getStatusColor(order.status),
+                ),
+                child: Center(
+                  child: Text(
+                    order.status,
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 15,
+                      color: whiteColor,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Divider(
+            thickness: 2,
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Total : '),
+              Text(
+                'Rp ${order.totalPrice.toStringAsFixed(0).replaceAll('.', ',')}',
+                style: primaryTextStyle.copyWith(
+                  fontWeight: semiBold,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 12,
           ),
         ],
       ),
     );
+  }
+}
+
+Color _getStatusColor(String status) {
+  switch (status) {
+    case "PENDING":
+      return Color.fromARGB(255, 219, 219, 4);
+    case "SUCCESS":
+      return Color.fromARGB(255, 36, 180, 41);
+    case "CANCELLED":
+      return Colors.red;
+    case "FAILED":
+      return Colors.red;
+    default:
+      return Colors.grey;
   }
 }
